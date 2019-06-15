23939e8c7f93ba12d98850f55ba4caf3a4e3c285
Pablo Rausch <pablorausch@gmail.com>
2019-06-15 00:07:49 -0300

    Agregar archivos de proyecto.

---
+++ Occurrences After Bigram/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Occurrences_After_Bigram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Occurrences After Bigram/Solution.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Occurrences_After_Bigram
+{
+    public class Solution
+    {
+        public string[] FindOcurrences(string text, string first, string second)
+        {
+            var pWord = "";
+            var ppWord = "";
+
+            var result = new List<string>();
+
+            foreach (var word in text.Split(" ", StringSplitOptions.RemoveEmptyEntries))
+            {
+                if (ppWord == first && pWord == second)
+                    result.Add(word);
+
+                ppWord = pWord;
+                pWord = word;
+            }
+
+            return result.ToArray();
+        }
+    }
+}
